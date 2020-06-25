4e40bc7dd6520079a8450aa18ff5902f0e036e33
Zach Hardy <zach.hardy@tamu.edu>
2020-06-25 10:44:44 -0500

    Convention changes

--- framework/problem.py
+++ framework/problem.py
@@ -3,151 +3,150 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from solvers.operator_splitting import OperatorSplitting
+
 class Problem:
-  """ Class for managing a problem.
+    """ Class for managing a problem.
 
-  This class is meant to solve general problems
-  in an agnostic way. What this means is that it 
-  depends on each physics module having similarly
-  named routines to perform higher level tasks such
-  as solving a time step, solving a steady state system,
-  etc.
-
-  Parameters
-  ----------
-  mesh : MeshBase
-  fields : list of Field
-  materials : list of MaterialBase
-  """
-  def __init__(self, mesh, materials):
-    self.mesh = mesh
-    self.materials = materials
-
-    # Physics
-    self.physics = []
-
-    # Fields
-    self.fields = []
-
-    # Solver
-    self.solver = None
-
-    # General info
-    self.n_dofs = 0
-    self.n_fields = 0
-    
-    # Solution vector
-    self.u = np.array([])
-
-    # Nonlinear parameters
-    self.tol = 1e-6
-    self.maxit = 100
-
-    # Time stepping parameters
-    self.method = None
-
-    # Verbosity
-    self.verbosity = 0
-
-  def RunSteadyState(self, tol=1e-6, maxit=100, verbosity=2):
-    """ Run a steady state problem.
+    This class is meant to solve general problems
+    in an agnostic way. What this means is that it 
+    depends on each physics module having similarly
+    named routines to perform higher level tasks such
+    as solving a time step, solving a steady state system,
+    etc.
 
     Parameters
     ----------
-    tol : float
-      Nonlinear convergence tolerance. Default is 1e-6.
-    maxit : int
-      Maximum nonlinear iteration. Default is 100.
-    verbosity : int
-      Level of screen printout. Default is 2.
+    mesh : mesh-like
+    fields : list of Fields
+    materials : list of material-like objects
     """
-    self.verbosity = verbosity
-    self.is_transient = False
-    # nonlinear parameters
-    self.tol = tol
-    self.maxit = maxit
-    # run problem
-    self.solver.SolveSystem()
+    def __init__(self, mesh, materials):
+        self.mesh = mesh
+        self.materials = materials
+        self.physics = []
+        self.fields = []
+        self.solver = None
+        # Field info
+        self.n_dofs = 0
+        self.n_fields = 0
+        # Solution vector
+        self.u = np.array([])
+        # Other parameters
+        self.tol = 1e-6
+        self.maxit = 100
+        self.verbosity = 0
 
-  def RunTransient(self, t0=0, tend=0.1, dt=2e-3, 
-           method='tbdf2', tol=1e-6, maxit=100,
-           verbosity=0):
-    """ Run a steady state problem.
+    def run_steady_state(
+            self, solver_type='os', tol=1e-6, 
+            maxit=100, verbosity=2):
+        """ Run a steady state problem.
 
-    Parameters
-    ----------
-    t0 : float
-      Simulation start time. Default is 0.
-    tend : float
-      Simulation end time. Default is 0.1.
-    dt : float
-      Simulation time step. Default is 2e-3.
-    method : str
-      The time stepping method. The options are
-      'fwd_euler', 'bwd_euler', 'cn', and 'tbdf2'.
-      Default is 'tbdf2'.
-    tol : float
-      Nonlinear convergence tolerance. Default is 1e-6.
-    maxit : int
-      Maximum nonlinear iteration. Default is 100.
-    verbosity : int
-      Level of screen printout. Default is 2.
-    """
-    self.verbosity = verbosity
-    self.is_transient = True
-    # store nonlinear parameters
-    self.tol = tol
-    self.maxit = maxit
-    # store time parameters
-    time = t0
-    dt = dt
-    step = 0
+        Parameters
+        ----------
+        solver_type : str, default=`os`
+            The type of solver to use. Currently, `os` is
+            the only allowable option.
+        tol : float
+            Nonlinear convergence tolerance. Default is 1e-6.
+        maxit : int
+            Maximum nonlinear iteration. Default is 100.
+        verbosity : int
+            Level of screen printout. Default is 2.
+        """
+        self.verbosity = verbosity
+        self.is_transient = False
+        # Nonlinear parameters
+        self.tol = tol
+        self.maxit = maxit
+        # Initialize a solver and solve.
+        self.solver = OperatorSplitting(self)
+        self.solver.SolveSystem()
 
-    # additional storage for half step
-    if method == 'tbdf2':
-      u_half = np.copy(self.u)
-    
-    # initial conditions
-    self.EvaluateICs()
-    self.u_old = np.copy(self.u)
+    def run_transient(
+            self, solver_type='os', t0=0, tend=0.1, 
+            dt=2e-3, method='tbdf2', tol=1e-6, maxit=100, 
+            verbosity=0):
+        """ Run a steady state problem.
 
-    # time stepping          
-    while time < tend:
-      step += 1
-      # print time step info
-      if self.verbosity > 0:
-        msg = "* Time Step {} *".format(step)
-        msg = "\n".join(["\n", "*"*len(msg), msg, "*"*len(msg)])
-        msg += "\ntime:\t{:.5f} micro-sec".format(time+dt)
-        msg += "\ndt:\t{:.5f} micro-sec".format(dt)
-        msg += "\n"+"="*25
-        print(msg)
+        Parameters
+        ----------
+        solver_type : str, default=`os`
+            The type of solver to use. Currently, `os` is
+            the only allowable option.
+        t0 : float
+            Simulation start time. Default is 0.
+        tend : float
+            Simulation end time. Default is 0.1.
+        dt : float
+            Simulation time step. Default is 2e-3.
+        method : str
+            The time stepping method. The options are
+            'fwd_euler', 'bwd_euler', 'cn', and 'tbdf2'.
+            Default is 'tbdf2'.
+        tol : float
+            Nonlinear convergence tolerance. Default is 1e-6.
+        maxit : int
+            Maximum nonlinear iteration. Default is 100.
+        verbosity : int
+            Level of screen printout. Default is 2.
+        """
+        self.verbosity = verbosity
+        self.is_transient = True
+        # Nonlinear parameters
+        self.tol = tol
+        self.maxit = maxit
+        # Initialize time stepping parameters.
+        time = t0
+        dt = dt
+        step = 0
+        # Create additional storage for mulit-step methods.
+        if method == 'tbdf2':
+            u_half = np.copy(self.u)
 
-      # solve the time step
-      if method != 'tbdf2':
-        self.solver.SolveSystem(time, dt, method)
-      else:
-        # half step crank nicholson
-        self.solver.SolveSystem(time, dt/2, 'cn')
-        u_half[:] = self.u # half step result
-        # half step BDF2
-        self.solver.SolveSystem(time+dt/2, dt/2, 'bdf2', u_half)
+        # Initialize a solver and start time stepping.
+        self.solver = OperatorSplitting(self)
+        self.evauluate_ics()
+        while time < tend:
+            step += 1
+            if self.verbosity > 0:
+                msg = "* Time Step {} *".format(step)
+                msg = "\n".join(["\n", "*"*len(msg), msg, "*"*len(msg)])
+                msg += "\ntime:\t{:.5f} micro-sec".format(time+dt)
+                msg += "\ndt:\t{:.5f} micro-sec".format(dt)
+                msg += "\n"+"="*25
+                print(msg)
 
-      # book-keeping 
-      time += dt
-      if time + dt > tend:
-        dt = tend - time
-      self.u_old[:] = self.u
+            # Handle single step methods.
+            if method != 'tbdf2':
+                self.solver.solve_system(time, dt, method)
 
-  def EvaluateICs(self):
-    """ Evaluate initial conditions. """
-    for physic in self.physics:
-      field = physic.field
-      for c in range(field.components):
-        dofs = field.ComponentDoFs(c)
-        grid = field.grid.ravel()
-        self.u[dofs] = physic.ics[c](grid)
-    self.u_old = np.copy(self.u)
+            # Handle multi-step methods. Currently, the only multi-step
+            # method available is TBDF2.
+            else:
+                # Take a half step with Crank Nicholson.
+                self.solver.solve_system(time, dt/2, 'cn')
+                # Store the result for use in BDF2.
+                u_half[:] = self.u 
+                # Take another half step with BDF2.
+                self.solver.solve_system(time+dt/2, dt/2, 'bdf2', u_half)
 
-      
-    
+            time += dt
+            # If the next time step pushes time beyond tend, change the
+            # time step to ensure the simulaltion ends at tend.
+            if time + dt > tend:
+                dt = tend - time
+            self.u_old[:] = self.u
+
+    def evauluate_ics(self):
+        """ Evaluate initial conditions. """
+        for physic in self.physics:
+            field = physic.field
+            for c in range(field.components):
+                dofs = field.component_dofs(c)
+                grid = field.grid.ravel()
+                self.u[dofs] = physic.ics[c](grid)
+        self.u_old = np.copy(self.u)
+
+            
+        
