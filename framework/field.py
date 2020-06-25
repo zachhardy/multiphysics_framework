d1d69e56c85a88bcef1fdc1f0ec021d2750003da
Zach Hardy <zach.hardy@tamu.edu>
2020-06-25 10:27:29 -0500

    Convention changes

--- framework/field.py
+++ framework/field.py
@@ -4,41 +4,41 @@ import sys
 import numpy as np
 
 class Field:
-  """ Field function object.
-  
-  Parameters
-  ----------
-  name : str
-    A name for this field function.
-  mesh : MeshBase
-    The mesh this field function is defined on.
-  discretization : Discretization
-    The discretization applied to this field
-  components : int, optional
-    The number of components this field has
-    (default is 1).
-  """
-  def __init__(self, name, mesh, discretization, components=1):
-    self.name = name
-    # Discretization information
-    self.mesh = mesh
-    self.sd = discretization
-    self.grid = discretization.grid
-    self.n_nodes = discretization.n_nodes
-    # Compnents information
-    self.components = components
-    self.n_dofs = components * self.n_nodes
-    # Global DoF information
-    self.dof_start = 0
-    self.dof_end = 0
+    """ Field function object.
+    
+    Parameters
+    ----------
+    name : str
+        A name for this field function.
+    mesh : MeshBase
+        The mesh this field function is defined on.
+    discretization : Discretization
+        The discretization applied to this field
+    components : int, optional
+        The number of components this field has
+        (default is 1).
+    """
+    def __init__(self, name, mesh, discretization, components=1):
+        self.name = name
+        # Discretization information
+        self.mesh = mesh
+        self.sd = discretization
+        self.grid = discretization.grid
+        self.n_nodes = discretization.n_nodes
+        # Compnents information
+        self.components = components
+        self.n_dofs = components * self.n_nodes
+        # Global DoF information
+        self.dof_start = 0
+        self.dof_end = 0
 
-  @property
-  def dofs(self):
-    """ Return the dofs for this field. """
-    return list(range(self.dof_start, self.dof_end))
-  
-  def component_dofs(self, component=0):
-    """ Return the dofs for a given component. """
-    start = self.dof_start + component*self.n_nodes
-    end = start + self.n_nodes
-    return self.dofs[start:end]
+    @property
+    def dofs(self):
+        """ Return the dofs for this field. """
+        return list(range(self.dof_start, self.dof_end))
+    
+    def component_dofs(self, component=0):
+        """ Return the dofs for a given component. """
+        start = self.dof_start + component*self.n_nodes
+        end = start + self.n_nodes
+        return self.dofs[start:end]
--- framework/material.py
+++ framework/material.py
@@ -4,121 +4,121 @@ import sys
 import numpy as np
 
 class MaterialBase:
-  """ Material base class. """
-  def __init__(self, material_id):
-    self.material_id = material_id
+    """ Material base class. """
+    def __init__(self, material_id):
+        self.material_id = material_id
 
 class NeutronicsMaterial(MaterialBase):
-  """ Class for a multigroup neutronics material. 
-    
-  There is much variability in neutronics properties. At a 
-  minimum, sig_r and either D or sig_t must be defined. Note
-  that if Na is provided, microscopic cross-sections should be 
-  specified, else macroscopic.
+    """ Class for a multigroup neutronics material. 
+        
+    There is much variability in neutronics properties. At a 
+    minimum, sig_r and either D or sig_t must be defined. Note
+    that if Na is provided, microscopic cross-sections should be 
+    specified, else macroscopic.
 
-  Parameters
-  ----------
-  Na : float, optional
-    The atom density (default is 1).
-  sig_r : list of float/callable
-    Removal cross section.
-  sig_t, D : list of float/callable
-    Total cross section, diffusion coefficient. Note that 
-    one must be provided. If D is not provided, it is
-    defaulted to 1/(3.sig_t).
-  chi, nu_sig_f : list of float/callable, optional
-    Fission spectrum, fission source cross section. Note 
-    that if a multigroup material, chi must be specified,
-    else is is 1.
-  sig_s : list of list of float/callable, optional
-    Scattering cross section.
-  v : list of float, optional
-    Group velocities.
-  q : list of float/callable, optional
-    Neutron source.
-  """
-  def __init__(self, material_id=0, Na=1., sig_r=[], sig_t=[], 
+    Parameters
+    ----------
+    Na : float, optional
+        The atom density (default is 1).
+    sig_r : list of float/callable
+        Removal cross section.
+    sig_t, D : list of float/callable
+        Total cross section, diffusion coefficient. Note that 
+        one must be provided. If D is not provided, it is
+        defaulted to 1/(3.sig_t).
+    chi, nu_sig_f : list of float/callable, optional
+        Fission spectrum, fission source cross section. Note 
+        that if a multigroup material, chi must be specified,
+        else is is 1.
+    sig_s : list of list of float/callable, optional
+        Scattering cross section.
+    v : list of float, optional
+        Group velocities.
+    q : list of float/callable, optional
+        Neutron source.
+    """
+    def __init__(self, material_id=0, Na=1., sig_r=[], sig_t=[], 
          D=[], sig_s=[], nu_sig_f=[],
          chi=[], v=[], q=[]):
-    super().__init__(material_id)
-    
-    # Atom density
-    self.Na = Na
+        super().__init__(material_id)
+        
+        # Atom density
+        self.Na = Na
 
-    # Removal cross section
-    assert sig_r != [], "sig_r must be specified."
-    self.sig_r = Na * np.atleast_1d(sig_r)
-    self.G = len(sig_r)
-    
-    # Total cross section
-    if sig_t != []:
-      assert len(sig_t)==self.G, "Invalid group structure."
-      self.sig_t = Na * np.atleast_1d(sig_t)
+        # Removal cross section
+        assert sig_r != [], "sig_r must be specified."
+        self.sig_r = Na * np.atleast_1d(sig_r)
+        self.G = len(sig_r)
+        
+        # Total cross section
+        if sig_t != []:
+            assert len(sig_t)==self.G, "Invalid group structure."
+            self.sig_t = Na * np.atleast_1d(sig_t)
 
-    # Diffusion coefficient
-    if D != []:
-      assert len(D)==self.G, "Invalid group structure."
-      self.D = np.atleast_1d(D)/Na 
-    else:
-      assert sig_t!=[], "If D is not specified, sig_t must be."
-      self.D = 1/(3*self.sig_t)
+        # Diffusion coefficient
+        if D != []:
+            assert len(D)==self.G, "Invalid group structure."
+            self.D = np.atleast_1d(D)/Na 
+        else:
+            assert sig_t!=[], "If D is not specified, sig_t must be."
+            self.D = 1/(3*self.sig_t)
 
-    # Scattering cross section
-    if sig_s != []:
-      sig_s = np.atleast_2d(sig_s)
-      assert sig_s.shape[0]==self.G, "Invalid group structure."
-      assert sig_s.shape[0]==sig_s.shape[1], "Invalid group structure."
-      self.sig_s = Na * sig_s
+        # Scattering cross section
+        if sig_s != []:
+            sig_s = np.atleast_2d(sig_s)
+            assert sig_s.shape[0]==self.G, "Invalid group structure."
+            assert sig_s.shape[0]==sig_s.shape[1], "Invalid group structure."
+            self.sig_s = Na * sig_s
 
-    # Fission cross sections
-    if nu_sig_f != []:
-      assert len(nu_sig_f)==self.G, "Invalid group structure."
-      self.nu_sig_f = Na * np.atleast_1d(nu_sig_f)
-      # Fission spectrum
-      if self.G > 1:
-        assert len(chi)==self.G, "Invalid group structure."
-        self.chi = np.atleast_1d(chi)
-      else:
-        self.chi = np.array([1.])
+        # Fission cross sections
+        if nu_sig_f != []:
+            assert len(nu_sig_f)==self.G, "Invalid group structure."
+            self.nu_sig_f = Na * np.atleast_1d(nu_sig_f)
+            # Fission spectrum
+            if self.G > 1:
+                assert len(chi)==self.G, "Invalid group structure."
+                self.chi = np.atleast_1d(chi)
+            else:
+                self.chi = np.array([1.])
 
-    # Velocity
-    if v != []:
-      assert len(v)==self.G, "Invalid group structure."
-      self.v = np.atleast_1d(v)
+        # Velocity
+        if v != []:
+            assert len(v)==self.G, "Invalid group structure."
+            self.v = np.atleast_1d(v)
 
-    # Source
-    if q != []:
-      assert len(q)==self.G, "Invalid group structure."
-      self.q = np.atleast_1d(q)
+        # Source
+        if q != []:
+            assert len(q)==self.G, "Invalid group structure."
+            self.q = np.atleast_1d(q)
 
 
 class HeatConductionMaterial(MaterialBase):
-  """ Class for a heat conduction material.
+    """ Class for a heat conduction material.
 
-  A heat conduction material is defined at a minimum
-  with a conductivity and can include specific heats 
-  and sources.
+    A heat conduction material is defined at a minimum
+    with a conductivity and can include specific heats 
+    and sources.
 
-  Parameters
-  ----------
-  k : float or callable
-    The thermal conductivity.
-  C_v : float or callable, optional
-    The specific heat. 
-  q : float or callable, optional
-    The heat source.
-  """
-  def __init__(self, material_id=0, k=[], C_v=[], q=[]):
-    super().__init__(material_id)
-    
-    # Conductivity
-    assert k != [], "k must be specified."
-    self.k = k
+    Parameters
+    ----------
+    k : float or callable
+        The thermal conductivity.
+    C_v : float or callable, optional
+        The specific heat. 
+    q : float or callable, optional
+        The heat source.
+    """
+    def __init__(self, material_id=0, k=[], C_v=[], q=[]):
+        super().__init__(material_id)
+        
+        # Conductivity
+        assert k != [], "k must be specified."
+        self.k = k
 
-    # Specific heat
-    if C_v != []:
-      self.C_v = C_v
-    
-    # Source
-    if q != []:
-      self.q = q
+        # Specific heat
+        if C_v != []:
+            self.C_v = C_v
+        
+        # Source
+        if q != []:
+            self.q = q
