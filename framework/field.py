bd48dcab0692301f5cf51194000247e67269691f
Zach Hardy <zach.hardy@tamu.edu>
2020-06-25 10:25:57 -0500

    Convention changes

--- framework/field.py
+++ framework/field.py
@@ -19,19 +19,15 @@ class Field:
     (default is 1).
   """
   def __init__(self, name, mesh, discretization, components=1):
-    # Field name
     self.name = name
-
     # Discretization information
     self.mesh = mesh
     self.sd = discretization
     self.grid = discretization.grid
     self.n_nodes = discretization.n_nodes
-    
     # Compnents information
     self.components = components
     self.n_dofs = components * self.n_nodes
-
     # Global DoF information
     self.dof_start = 0
     self.dof_end = 0
@@ -41,7 +37,7 @@ class Field:
     """ Return the dofs for this field. """
     return list(range(self.dof_start, self.dof_end))
   
-  def ComponentDoFs(self, component=0):
+  def component_dofs(self, component=0):
     """ Return the dofs for a given component. """
     start = self.dof_start + component*self.n_nodes
     end = start + self.n_nodes
