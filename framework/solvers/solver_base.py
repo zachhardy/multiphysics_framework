38c9a63adef14521084fc42159818466b98b7d74
Zach Hardy <zach.hardy@tamu.edu>
2020-06-25 10:50:36 -0500

    Convention changes

--- framework/solvers/solver_base.py
+++ framework/solvers/solver_base.py
@@ -1,21 +1,21 @@
 #/usr/bin/env python3
 
 class SolverBase:
-  """ Base class for multiphysics solvers.
+    """ Base class for multiphysics solvers.
 
-  Parameters
-  ----------
-  problem : Problem
-  """
-  def __init__(self, problem):
-    self.problem = problem
+    Parameters
+    ----------
+    problem : Problem
+    """
+    def __init__(self, problem):
+        self.problem = problem
 
-  def old_physics_action(self):
-    """ Compute the old physics action for each physics. """
-    for physics in self.problem.physics:
-      physics.OldPhysicsAction()
+    def old_physics_action(self):
+        """ Compute the old physics action for each physics. """
+        for physics in self.problem.physics:
+            physics.OldPhysicsAction()
 
-  def solve_system(self):
-    raise NotImplementedError
+    def solve_system(self):
+        raise NotImplementedError
 
-  
+    
