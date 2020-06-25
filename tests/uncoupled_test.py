c31e94c5ab9b1baed937a82a6fdaa7cc0f91595b
Zach Hardy <zach.hardy@tamu.edu>
2020-06-25 14:03:41 -0500

    Tests brought to compatibility

--- tests/uncoupled_test.py
+++ tests/uncoupled_test.py
@@ -7,12 +7,11 @@ import matplotlib.pyplot as plt
 sys.path.extend(['../framework', '../modules'])
 from problem import Problem
 from mesh.mesh import Mesh1D
-from field import Field
-from material import HeatConductionMaterial
-from material import NeutronicsMaterial
 from bc import BC
+from heat_conduction.hc_material import HeatConductionMaterial
 from heat_conduction.cfe_hc import CFE_HeatConduction
 from mg_diffusion.cfe_mg_diffusion import CFE_MultiGroupDiffusion
+from mg_diffusion.neutronics_material import NeutronicsMaterial
 
 def k(T):
     return 1.5 + (2510 / (215 + T))
@@ -33,14 +32,14 @@ problem = Problem(mesh, materials)
 
 ### Physics
 # neutronics
-phi_bcs = [BC('neumann', 0, [0]), BC('robin', 1, [[0.5],[2.]])]
+phi_bcs = [BC('robin', 0, [[0.5],[0]]), BC('robin', 1, [[0.5],[0]])]
 mgd = CFE_MultiGroupDiffusion(problem, 1, phi_bcs)
 # conduction
-T_bcs = [BC('neumann', 0, 0), BC('dirichlet', 1, 300.)]
+T_bcs = [BC('dirichlet', 0, 300), BC('dirichlet', 1, 300.)]
 hc = CFE_HeatConduction(problem, T_bcs)
 
 ### Run
-problem.RunSteadyState()
+problem.run_steady_state()
 
 plt.figure(0)
 plt.plot(hc.field.grid, hc.u)
