d2266a298b0e7ab98ba1e36a824c1e347bf7a061
Zach Hardy <zach.hardy@tamu.edu>
2020-07-13 15:23:59 -0500

    Small change

--- framework/solvers/operator_splitting.py
+++ framework/solvers/operator_splitting.py
@@ -12,14 +12,14 @@ class OperatorSplitting(SolverBase):
     self.u = problem.u
     self.u_ell = np.copy(problem.u_ell)
     
-  def solve_system(self, *args):
+  def solve_system(self, opt=0):
     # Initialize u_ell with current solution
     self.u_ell[:] = self.u
     # Nonlinear iterations
     diff, nit = 1., 0
     while diff > self.tol and nit < self.maxit:
       for physic in self.problem.physics:
-        physic.solve_system(*args)
+        physic.solve_system(opt)
       diff = np.linalg.norm(self.u-self.u_ell, ord=2)
       self.u_ell[:] = self.u
       nit += 1
